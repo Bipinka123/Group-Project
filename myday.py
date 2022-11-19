1b09b70ec6bc2376d6b89b313be792405365abb7
Anol Saha <anolsaha@Anols-MacBook-Pro.local>
2022-11-19 13:20:09 -0600

    login progress

--- myday.py
+++ myday.py
@@ -1,1 +1,17 @@
+import json
+import requests
 import flask
+from dotenv import load_dotenv, find_dotenv
+import os
+import random
+from flask_sqlalchemy import SQLAlchemy
+
+
+load_dotenv(find_dotenv())
+app = flask.Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+db = SQLAlchemy(app)
+
+@app.route('/login')
+def loginPage():
+    return flask.render_template('login.html')
