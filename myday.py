58fa8e6a1bf12b2a9629bda401e1b7202e32c3c3
Bipin Kadariya <bipin.ka123@gmail.com>
2022-11-24 17:12:38 -0600

    top 10 news and best selling books completed

--- myday.py
+++ myday.py
@@ -1,15 +1,14 @@
+"""import os"""
 import os
 import flask
 from dotenv import load_dotenv, find_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from flask import redirect, url_for, request, flash
 import requests
-import json
 from flask_login import (
     login_user,
     login_required,
     UserMixin,
-    login_manager,
     LoginManager,
     logout_user,
     current_user,
@@ -30,15 +29,15 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
+    """load user"""
     return Persons.query.get(int(user_id))
 
 
 class Persons(UserMixin, db.Model):
+    """create table"""
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-
-    def __repr__(self):
-        return "<User %r>" % self.username
 
 
 with app.app_context():
@@ -46,33 +45,35 @@ with app.app_context():
 
 
 @app.route("/")
-def signupPage():
+def signup_page():
+    "define signup page"
     return flask.render_template("signup.html")
 
 
 @app.route("/signup", methods=["GET", "POST"])
-def handleSignup():
+def handle_signup():
+    """handle signup page"""
     user_name = request.form.get("Username")
     newUser = Persons(username=user_name)
     user = Persons.query.filter_by(username=user_name).first()
 
     if user:
         flash("Username exists. Please enter different user or click login")
-        return redirect(url_for("signupPage"))
+        return redirect(url_for("signup_page"))
 
     db.session.add(newUser)
     db.session.commit()
 
-    return redirect(url_for("loginPage"))
+    return redirect(url_for("login_page"))
 
 
 @app.route("/news")
+@login_required
 def top_ten_news():
-    NYT_ARTICLE_SEARCH_API_BASE_URL = (
-        "https://api.nytimes.com/svc/search/v2/articlesearch.json"
-    )
+    """define news function"""
+    nyt_search_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
     response = requests.get(
-        NYT_ARTICLE_SEARCH_API_BASE_URL,
+        nyt_search_url,
         params={
             "api-key": os.getenv("NYT_ARTICLE_SEARCH_API_KEY"),
         },
@@ -89,38 +90,62 @@ def top_ten_news():
     )
 
 
+@app.route("/top selling books")
+@login_required
+def top_selling_books():
+    """define current best selling books"""
+    nyt_best_selling_book_url = (
+        "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json"
+    )
+
+    response = requests.get(
+        nyt_best_selling_book_url,
+        params={
+            "api-key": os.getenv("NYT_ARTICLE_SEARCH_API_KEY"),
+        },
+        timeout=10,
+    )
+
+    json_data = response.json()
+    books_objects = json_data["results"]["books"]
+    user = current_user.username
+    return flask.render_template("books.html", books_objects=books_objects, user=user)
+
+
 @app.route("/login")
-def loginPage():
+def login_page():
+    """login page"""
     return flask.render_template("login.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
-def handleLogin():
+def handle_login():
     """handle login form"""
     user_name = request.form.get("Username")
-    checkUser = Persons.query.filter_by(username=user_name).first()
+    checkuser = Persons.query.filter_by(username=user_name).first()
 
-    if not checkUser:
+    if not checkuser:
         flash("Username does not exist. Please signup or try again!")
-        return redirect(url_for("loginPage"))
+        return redirect(url_for("login_page"))
     else:
-        login_user(checkUser)
-        return redirect(url_for("mainPage"))
+        login_user(checkuser)
+        return redirect(url_for("main_page"))
 
 
 @app.route("/main", methods=["GET", "POST"])
 @login_required
-def mainPage():
-
+def main_page():
+    """define mainpage"""
     user = current_user.username
     return flask.render_template("main.html", user=user)
 
 
 @app.route("/logout", methods=["GET", "POST"])
 @login_required
-def handleLogout():
+def handle_logout():
+    """handling logout"""
     logout_user()
-    return redirect(url_for("loginPage"))
+    return redirect(url_for("login_page"))
 
 
 app.run(debug=True)
